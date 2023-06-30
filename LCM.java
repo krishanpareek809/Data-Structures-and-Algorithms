cf529605ba606aabaad3220945b933b2eabc3f59
Krishan Pareek <krishanpareek809@gmail.com>
2023-06-30 23:00:28 +0530

    LCM of two numbers

---
+++ LCM.java
@@ -0,0 +1,17 @@
+public class LCM {
+    static int lcm(int a, int b){
+        int res = Math.max(a, b);
+        while(true){
+            if(res%a==0 && res%b==0){
+                return res;
+            }
+            res++;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lcm(4,6));
+    }
+}
+
+// Time Complexity = O(a*b - max(a,b))
