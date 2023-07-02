3c9b46029f2f5c2aaf2dabdd2d8b348648708841
Krishan Pareek <krishanpareek809@gmail.com>
2023-07-02 14:39:27 +0530

    Prime Factors of a number

---
+++ PrimeFactors.java
@@ -0,0 +1,28 @@
+public class PrimeFactors {
+    
+    static boolean isPrime(int n){
+        if(n==1) return false;
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void primeFactors(int n){
+        for(int i=2; i<n; i++){
+            if(isPrime(i)){
+                int x = i;
+                while(n%x==0){
+                    System.out.println(i);
+                    x = x*i;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        primeFactors(12);
+    }
+}
