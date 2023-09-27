d459bd0ef3f7e3e59adb1b33adaac00169ffb492
Krishan Pareek <krishanpareek809@gmail.com>
2023-09-27 18:42:55 +0530

    Efficient method for printing all divisors of a number

--- PrintDivisors.java
+++ PrintDivisors.java
@@ -1,3 +1,4 @@
+/*
 public class PrintDivisors {
     
     static void printDivisors(int n){
@@ -12,6 +13,39 @@ public class PrintDivisors {
         printDivisors(25);
     }
 }
+*/
 
 // Time Complexity = Theta(n)
 // Auxiliary Space = Theta(1)
+
+
+// Efficient Method 
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class PrintDivisors {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n to print it's all divisors: ");
+        int n = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=1;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                list.add(i);
+                if(n/i!=i){
+                    list.add(n/i);
+                }
+            }
+        }
+        Collections.sort(list);
+        for(int element: list){
+            System.out.print(element+" ");
+        }
+    }
+}
+
+// Time Complexity = O(sqrt(n))
